f7ec17bf95d8ba23c0ec98dd6152276188ac1a76
bn1 <petr@vitecek.cz>
2019-07-28 19:03:56 +0200

    Další krok
    Tady by klidně mohl být delší *.md text, _to by bylo_!
    
     * jo, líbilo by se mi to!
     * no jasně!

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import React from "react";
+import Footer from "./Footer";
+import AddTodo from "../containers/AddTodo";
+import VisibleTodoList from "../containers/VisibleTodoList";
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
+    <h2>an element</h2>
     <Footer />
   </div>
-)
+);
 
-export default App
+export default App;
